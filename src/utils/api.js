e4a52f91216e9c17882e3f65f24da70bdb58da00
Shusbin Tripathi <shusbin.king32@gmail.com>
2025-07-02 15:51:22 +0545

    fixed env problem

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,11 +1,14 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.NODE_ENV ==='production'? `https://e-commerce-site-backend-hx6v.onrender.com/api`:"http://localhost:10000/api", // your backend URL
+  // eslint-disable-next-line no-undef
+  baseURL: process.env.NODE_ENV ==='production'? `https://e-commerce-site-backend-hx6v.onrender.com/api`:"http://localhost:10000/api", // your backend URL
 });
-console.log('API Base URL:', import.meta.env.VTIE_BACKEND_BASE_URL);
 
-// Auto attach token to each request if exists
+// eslint-disable-next-line no-undef
+console.log('API Base URL:', process.env.NODE_ENV);
+
+// Auto attach token to each request if existsclear
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem('token');
   if (token) config.headers.Authorization = `Bearer ${token}`;
